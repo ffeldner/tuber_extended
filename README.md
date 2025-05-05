64deba375f54833d7d89965c24b01a8c9981ae81
Flora Elisabeth Feldner <ffeldner@gmx.at>
2025-05-05 05:52:17 +0200

    first commit of extension with support for vtt subtitle and presentation/slide download. fixed lecture episode limit (up to 2000 from 100). fallback deduplicated

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# tuber - tube.tugraz.at downloader
+# tuber_extended - tube.tugraz.at downloader
+
+now with support for downloading presentation slide video track and vtt subtitles. also now supports more than only the first 100 episodes in a given lecture series (up to 2000). fallback is more robust and deduplicated now.
+
+huge thanks to daev for creating this
 
 ## requirements
 
@@ -15,23 +19,14 @@ tuber expects some environment variables:
 
 * USER and PASS contain the credentials to logon to tube.tugraz.at
 * COURSE contains the course-uuid to download
-* RESOLUTION contains the quality to download (defaults to 1920x1080 if left unset)
 
-### example using docker
+you can also set the following additional environment variables:
+NOPRESENTATIONSLIDES and NOSUBTITLES to 0 or 1 respectively to control the download of the presentation/slides video file as well as the subtitles that are present in newer lectures in vtt format.
 
-you can use the prebuilt image from dockerhub to download analysis 1 to /tmp
-
-    docker run --rm -v /tmp:/data -e USER=your_user -e PASS=your_pass -e COURSE=4636c0b6-71a8-45f1-bc6a-ea850f46175e daef/tuber
+default setting is no slides but subtitles when existant
 
 ### example without docker
 
 to download analysis 1 to the current directory
 
     USER=your_user PASS=your_pass COURSE=4636c0b6-71a8-45f1-bc6a-ea850f46175e /path/to/tuber.sh
-
-
-### Nix (with flakes)
-
-to download analysis 1 to the current directory run
-
-    USER=your_user PASS=your_pass COURSE=4636c0b6-71a8-45f1-bc6a-ea850f46175e nix run github:daef/tuber
--- tuber.sh
+++ tuber.sh
@@ -2,15 +2,25 @@
 
 : "${USER:=user}"
 : "${PASS:=pass}"
-: "${COURSE:=4636c0b6-71a8-45f1-bc6a-ea850f46175e}"
-: "${RESOLUTION:=1920x1080}"
-: "${FALLBACKRES:=1440x900}"
+: "${COURSE:=0bcae57f-740a-4033-b560-e9747ee80d78}"
+: "${QUALITY_PRESENTER:=high}"
+: "${QUALITY_PRESENTATION:=high}"
+: "${FALLBACK_RESOLUTION:=1280x720}"
+: "${NOPRESENTATIONSLIDES:=1}"
+: "${NOSUBTITLES:=0}"
+
+if [[ $NOSUBTITLES == 1 ]] ; then
+	echo "Subtitle Download disabled..."
+fi
+if [[ $NOPRESENTATIONSLIDES == 1 ]] ; then
+	echo "Presentation/Slides Download disabled..."
+fi
 
 CURL="curl -L -c cookies -b cookies"
 CURL_STDOUT="$CURL -s -o -"
 TUBE="https://tube.tugraz.at"
 INITURL="$TUBE/Shibboleth.sso/Login?target=/paella/ui/index.html"
-EPIURL="$TUBE/search/episode.json?sid=$COURSE"
+EPIURL="$TUBE/search/episode.json?limit=2000&offset=0&sid=$COURSE"
 
 RESPONSE=$($CURL_STDOUT -L -c cookies -b cookies -s -o - ${INITURL})
 if [[ ! $RESPONSE =~ "Welcome to TU Graz TUbe" ]] ; then
@@ -25,51 +35,95 @@ fi
 echo logged in
 echo $EPIURL
 $CURL -s -o episodes.json "$EPIURL"
+
+SERIESTITLE="$(cat episodes.json | jq -c "[.[\"search-results\"].result[].mediapackage| {seriestitle: .seriestitle}] | unique[0] | .seriestitle"| tr -d '[:punct:]')"
+
+mkdir -p "$SERIESTITLE"
+
 cat episodes.json | jq -c "
 	.[\"search-results\"]
 	.result[]
 	.mediapackage
 	| {
+
+		date: .start,
 		title: .title,
-		urls: [ .media.track[]
-			| select(.video.resolution == \"$RESOLUTION\")
+		presenter: [ .media.track[]
+			| select(.mimetype==\"video\/mp4\")
+			| select(.type==\"presenter\/delivery\")
+			| select(.tags.tag[]==\"$QUALITY_PRESENTER\" or .tags.tag[]==\"1080p-quality\")
+			| .url],
+		presentation: [ .media.track[]
+			| select(.mimetype==\"video\/mp4\")
+			| select(.type==\"presentation\/delivery\")
+			| select(.tags.tag[]==\"$QUALITY_PRESENTATION\" or .tags.tag[]==\"1080p-quality\")
+			| .url],
+		fallback_presenter: [ .media.track[]
+			| select(.mimetype==\"video\/mp4\")
+			| select(.type==\"presenter\/delivery\")
+			| select(.video.resolution==\"$FALLBACK_RESOLUTION\" or .tags.tag[]==\"720p-quality\")
+			| .url],
+		fallback_presentation: [ .media.track[]
+			| select(.mimetype==\"video\/mp4\")
+			| select(.type==\"presentation\/delivery\")
+			| select(.video.resolution==\"$FALLBACK_RESOLUTION\" or .tags.tag[]==\"720p-quality\")
+			| .url],
+		vtt: [ .attachments.attachment[]
+			| select(.mimetype==\"text\/vtt\")
 			| .url]
 	}" |
 		while read episode
 do
+	DATE="$(echo "$episode" | jq -r .date)"
 	TITLE="$(echo "$episode" | jq -r .title)"
-	FN="$(echo "$TITLE" | tr -dc 'a-zA-Z0-9' ).mp4"
-	URL="$(echo "$episode" | jq -r .urls[0])"
-  echo episodeurl $URL
-  if [ "$URL" != "null" ] ; then
-    if [ ! -f "$FN" ] ; then
-      echo "downloading $TITLE to $FN"
-      $CURL -C - -o "$FN.part" "$URL"
+	FN="$SERIESTITLE/${DATE:0:10}_$(echo "$TITLE" | tr -dc 'a-zA-Z0-9' ).mp4"
+	FNS="$SERIESTITLE/${DATE:0:10}_$(echo "$TITLE" | tr -dc 'a-zA-Z0-9' )_Slides.mp4"
+	FNV="$SERIESTITLE/${DATE:0:10}_$(echo "$TITLE" | tr -dc 'a-zA-Z0-9' ).vtt"
+	URLPER="$(echo "$episode" | jq -r .presenter[0])"
+	URLPION="$(echo "$episode" | jq -r .presentation[0])"
+	URLVTT="$(echo "$episode" | jq -r .vtt[0])"
+
+	echo -e "\n--- Episode $TITLE from ${DATE:0:10}"
+
+	if [[ "$URLPER" == "null" ]] ; then
+		URLPER="$(echo "$episode" | jq -r .fallback_presenter[0])"
+		if [ "$URLPER" != "null" ] ; then
+			echo "Falling back to medium quality for Presenter"
+		else
+			echo "No URL found for Presenter"
+		fi
+	fi
+	if [[ $NOPRESENTATIONSLIDES == 0 && "$URLPION" == "null" ]] ; then
+		URLPION="$(echo "$episode" | jq -r .fallback_presentation[0])"
+		if [ "$URLPION" != "null" ] ; then
+			echo "Falling back to medium quality for Presentation/Slides"
+		else
+			echo "No URL found for Presentation/Slides"
+		fi
+	fi
+	if [[ $NOSUBTITLES == 0 && "$URLVTT" == "null" ]] ; then
+		echo "No URL found for Subtitles - This is normal for content from before 2023"
+	fi
+  echo "Presenter URL: " $URLPER" | Presentation URL: " $URLPION" | Subtitle URL: " $URLVTT
+  if [[ "$URLPER" != "null" ]] ; then
+    if [[ ! -f "$FN" ]] ; then
+      echo "downloading Presenter to $FN"
+      $CURL -C - -o "$FN.part" "$URLPER"
       mv "$FN"{.part,}
     fi
   fi
+  if [[ $NOPRESENTATIONSLIDES == 0 && "$URLPION" != "null" ]] ; then
+    if [[ ! -f "$FNS" ]] ; then
+      echo "downloading Presentation to $FNS"
+      $CURL -C - -o "$FNS.part" "$URLPION"
+      mv "$FNS"{.part,}
+    fi
+  fi
+  if [[ $NOSUBTITLES == 0 && "$URLVTT" != "null" ]] ; then
+    if [[ ! -f "$FNV" ]] ; then
+      echo "downloading Subtitles to $FNV"
+      $CURL -C - -o "$FNV.part" "$URLVTT"
+      mv "$FNV"{.part,}
+    fi
+  fi
 done
-
-cat episodes.json | jq -c "
-	.[\"search-results\"]
-	.result[]
-	.mediapackage
-	| {
-		title: .title,
-		urls: [ .media.track[]
-			| select(.video.resolution == \"$FALLBACKRES\")
-			| .url]
-	}" |
-		while read episode
-do
-	TITLE="$(echo "$episode" | jq -r .title)"
-	FN="$(echo "$TITLE" | tr -dc 'a-zA-Z0-9' ).mp4"
-	URL="$(echo "$episode" | jq -r .urls[0])"
-  echo fallback episodeurl $URL
-	if [ ! -f "$FN" ] ; then
-		echo "downloading $TITLE to $FN"
-		$CURL -C - -o "$FN.part" "$URL"
-		mv "$FN"{.part,}
-	fi
-done
-
